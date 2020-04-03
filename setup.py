ddd9e4843e67eb044cad85efffbedff9ebb4cb10
ctgk <r1135nj54w@gmail.com>
2020-04-04 02:00:18 +0900

    add some info about this project

--- setup.py
+++ setup.py
@@ -11,13 +11,29 @@ develop_requires = [
 ]
 
 
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
 setup(
     name='pyautocast',
     version=__version__,
     author='ctgk',
     author_email='r1135nj54w@gmail.com',
+    maintainer='ctgk',
+    maintainer_email='r1135nj54w@gmail.com',
     url='https://github.com/ctgk/pyautocast',
     description='Automatic function arguments casting',
+    long_description=readme,
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+    ],
 
     packages=find_packages(exclude=('test*',)),
     python_requires=">=3.7",
